3d93dd0559d728e131e71aebf7238d2023ded492
“hillmclean” <hilldmc@gmail.com>
2020-04-28 18:11:03 -0700

    adds second image to project

--- themes/stepup4earth-theme/template-parts/content-projects.php
+++ themes/stepup4earth-theme/template-parts/content-projects.php
@@ -8,10 +8,11 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class('workshop-no-owner'); ?>>
-<?php if( get_field('image_1') ): ?>
-		<div class="workshop-title-image" style="background: url('<?php the_field('image_1') ?>') no-repeat; background-size: cover; background-position: center;">
-		<?php endif; ?>
-	</div>
+
+		<div class="workshop-title-image" style="background: url('<?php the_field('image_1') ?>') no-repeat; background-size: cover; background-position: center;"></div>
+		<div class="workshop-secondary-image" style="background-image: url(<?php echo get_field("image_2") ?>)"></div>
+	
+ 
 	<header class="entry-header">
 		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 	</header><!-- .entry-header -->
