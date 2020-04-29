b6f6476df1d07ea89ce5f090c595f9653f5c51ef
“hillmclean” <hilldmc@gmail.com>
2020-04-28 18:05:20 -0700

    sorts and displays custom amount of posts on projects main page

--- themes/stepup4earth-theme/archive-projects.php
+++ themes/stepup4earth-theme/archive-projects.php
@@ -13,35 +13,41 @@ get_header(); ?>
 		<?php if ( have_posts() ) : ?>
 
 			<?php
-				$args = array( 
-					'post_type' => 'post',
-					'posts_per_page' => '1'
-				);
-				$projects = get_posts( $args ); 
-					?>
-
-			<header class="page-header-archive">
-				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
-					the_archive_description( '<div class="taxonomy-description">', '</div>' );
-				?>
+			    $args = array( 
+				'post_type'       => 'page', 
+				'posts_per_page'  => 1,
+				'name'           => 'projects',
+			    );
+			    $projects_page = get_posts( $args ); // returns an array of posts
+			
+			?>
+			<div class='workshops-container'>
+			<?php foreach ( $projects_page as $post ) : setup_postdata( $post ); ?>
+			<header class="page-header-content">
+			<div class="workshop-page-content">
+					<h2><?php the_title(); ?></h2>
+					<?php the_content(); ?>
+				    </div>
 			</header><!-- .page-header -->
+			<?php endforeach; wp_reset_postdata(); ?>   
 
 			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php
-					get_template_part( 'template-parts/content-projects' );
-				?>
-
-			<?php endwhile; ?>
-
+			
+			<?php
+			    $args = array( 
+				'post_type'       => 'projects', 
+				'posts_per_page'  => 3,
+				'orderby'         => 'date',
+				'order'           => 'ASC',
+			    );
+			    $products_post = get_posts( $args ); // returns an array of posts
+			?>
+			<?php foreach ( $products_post as $post ) : setup_postdata( $post ); ?>
+			<?php include( locate_template( 'template-parts/content-projects.php', false) ); ?>
+			<?php endforeach ; ?>
 			<?php the_posts_navigation(); ?>
 
-		<?php else : ?>
-
-			<?php get_template_part( 'template-parts/content', 'none' ); ?>
-
+			
 		<?php endif; ?>
 
 					
