0de3e27b3a1b94ff6de803442b9beb68b35c7a60
“hillmclean” <hilldmc@gmail.com>
2020-04-28 22:41:38 -0700

    adds pop-up banner

--- themes/stepup4earth-theme/header.php
+++ themes/stepup4earth-theme/header.php
@@ -19,9 +19,9 @@
 	<body <?php body_class(); ?>>
 		<div id="page" class="hfeed site">
 			<a class="skip-link screen-reader-text" href="#content"><?php echo esc_html( 'Skip to content' ); ?></a>
-
 			<header id="masthead" class="site-header" role="banner">
-				<div class="site-branding">
+	
+			<div class="site-branding">
 				<a href="<?php echo get_home_url() ?>">
 				    <img src="<?php echo get_template_directory_uri() . "/build/assets/logos/GlobeLogo.svg"?>"></img>
 				</a>
